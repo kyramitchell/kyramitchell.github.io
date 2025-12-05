5e02ec8edc3b3a0dc969c35423aa613d8a5e777e
kyramitchell <kyra.mitchell.banire@gmail.com>
2025-12-05 01:56:36 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-function toggleVideo(id) {
-  const vid = document.getElementById(id);
-
-  if (vid.classList.contains("hidden")) {
-    vid.classList.remove("hidden");
-  } else {
-    vid.classList.add("hidden");
-  }
-}
-
 function scrollToWork() {
   document.getElementById("work").scrollIntoView({ behavior: "smooth" });
 }
